8e72ec6f789ee7ab5bf54dade38e80c1642431a4
MyZetdizgit <bambizazizo@gmail.com>
2024-08-31 20:18:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const axios = require('axios');
+const request = require('request');
 const fs = require('fs');
 const path = require('path');
-const { Prodia } = require('prodia.js');
 
 const app = express();
 const port = 3000;
@@ -13,7 +12,7 @@ const upscaleModels = [
   "ESRGAN_SRx4"
 ];
 
-app.get('/upscale', async (req, res) => {
+app.get('/upscale', (req, res) => {
   const { imageUrl, modelIndex = 0, resize = 2 } = req.query;
 
   if (!imageUrl) {
@@ -25,32 +24,37 @@ app.get('/upscale', async (req, res) => {
     return res.status(400).send('Invalid model index');
   }
 
-  try {
-    const { upscale } = Prodia("d80b712c-028b-4ba7-a9cf-111f59fb1e7b");
-
-    const upscaleResult = await upscale({
+  const options = {
+    method: 'POST',
+    url: 'https://api.prodia.com/v1/upscale',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json'
+    },
+    body: {
       resize: parseInt(resize, 10),
       model: upscaleModels[parsedModelIndex],
       imageUrl
-    });
+    },
+    json: true
+  };
 
-    if (upscaleResult.status !== "succeeded") {
-      console.error("Upscaling failed");
-      return res.status(500).send("Upscaling failed");
+  request(options, function (error, response, body) {
+    if (error) {
+      console.error(error);
+      return res.status(500).send("An error occurred during the upscaling process");
     }
 
-    const imagePath = path.join(__dirname, 'cache', 'upscaled.png');
-    const response = await axios.get(upscaleResult.imageUrl, { responseType: 'stream' });
-
-    response.data.pipe(fs.createWriteStream(imagePath))
-      .on('finish', () => {
-        return res.sendFile(imagePath);
-      });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("An error occurred while processing the image upscaling");
-  }
+    if (body && body.url) {
+      const imagePath = path.join(__dirname, 'cache', 'upscaled.png');
+      request(body.url)
+        .pipe(fs.createWriteStream(imagePath))
+        .on('close', () => res.sendFile(imagePath));
+    } else {
+      console.error("Failed to upscale the image");
+      res.status(500).send("Upscaling failed");
+    }
+  });
 });
 
 app.listen(port, () => {
